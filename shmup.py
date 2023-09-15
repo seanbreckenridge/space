77b504da5713e035b0f3fb110cfcec64be594ab7
Sean Breckenridge <seanbrecke@gmail.com>
2023-09-14 20:47:37 -0700

    remove unused vars

--- shmup.py
+++ shmup.py
@@ -12,10 +12,6 @@ FPS = 60
 # colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
 
 # global constants
 START_ASTEROID_COUNT = 13
